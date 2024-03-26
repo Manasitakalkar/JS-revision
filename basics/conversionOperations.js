977c7d286b087cbbbaf4d22530f4d31634aa4f20
Manasi Takalkar <manasivtakalkar@gmail.com>
2024-03-26 14:57:09 +0530

    conversions

---
+++ basics/conversionOperations.js
@@ -0,0 +1,27 @@
+//let score = "33"
+//let score = "33abcd"
+//let score = null
+//let score = undefined
+let score = true
+
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let newNum = Number(score);
+//console.log(typeof newNum);
+console.log(newNum)
+
+
+let isLoggedIn = 1
+let conv = Boolean(isLoggedIn);
+console.log(conv);
+
+
+// 1 = true, 0 = false
+// "" - false , "manasi" = true
+
+let somenum = 44
+let s = String(somenum);
+console.log(s);  //44
+console.log(typeof s)  // string
