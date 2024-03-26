9cd4f3e5fd5c12072715dbd224cd12612c6359b9
Manasi Takalkar <manasivtakalkar@gmail.com>
2024-03-26 22:18:02 +0530

    conversions

---
+++ basics/comparison.js
@@ -0,0 +1,24 @@
+// console.log("02" >3);
+// console.log("2" >3);
+// console.log(2 >3);
+// console.log("02" < 3);
+
+// comparison and equality check are two different things
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+
+console.log("2" === 2);
+
+
+console.log(null > 0)
+
+
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -10,12 +10,12 @@ let score = true
 
 let newNum = Number(score);
 //console.log(typeof newNum);
-console.log(newNum)
+//console.log(newNum)
 
 
 let isLoggedIn = 1
 let conv = Boolean(isLoggedIn);
-console.log(conv);
+//console.log(conv);
 
 
 // 1 = true, 0 = false
@@ -23,5 +23,22 @@ console.log(conv);
 
 let somenum = 44
 let s = String(somenum);
-console.log(s);  //44
-console.log(typeof s)  // string
+// console.log(s);  //44
+// console.log(typeof s)  // string
+
+
+// ********************* Operations ***********************
+
+let value = 4
+let negvalue = -value
+console.log(negvalue)
+
+console.log("1" + 2)
+console.log(1 + "3")
+console.log(1 + 3+ "3")
+console.log("5" + 3+ 6)
+
+console.log(+true)
+console.log( +false)
+
+
