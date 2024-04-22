35f9a3ec435eb0b89ea54d72964dcdbb72bf1c72
Manasi Takalkar <manasivtakalkar@gmail.com>
2024-04-22 12:38:27 +0530

    loops

---
+++ 05_loops/04_four.js
@@ -0,0 +1,33 @@
+const myObject = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+ //console.log(key);  
+ //console.log(myObject[key]);
+// console.log(`${key} is a shortcut for ${myObject[key]}`);
+}
+
+
+const myArray = ["a", "b", "c"];
+
+for (const key in myArray) {
+    // console.log(key);
+    // console.log(myArray[key]);
+
+}
+
+
+// const map = new Map();
+// map.set('IN', 'India');
+// map.set('uk', 'united kingdom')
+// map.set('Arb', 'arabia')
+// map.set('us', 'united srtates')
+// map.set('uk', 'united kingdom')
+
+// for (const key in map) {
+// console.log(key);
+// } 
---
+++ 05_loops/05_five.js
@@ -0,0 +1,5 @@
+const coding =["js", "ruby", "python","cpp"];
+
+coding.forEach(function (val){
+    console.log(val);
+})
